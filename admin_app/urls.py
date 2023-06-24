aa4682373d3a4da17033c1f0cf9fa5d795db1baf
Dimitri Edel <dimitri.edel@gmx.net>
2023-06-24 16:11:13 +0200

    Add admin_app > url:admin_workout_list view: WorkoutList template: admin_workout_list.html

--- admin_app/urls.py
+++ admin_app/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("home/", views.UserList.as_view(), name="admin-home"),
+    path("admin_workout_list/<int:user_id>", views.WorkoutList.as_view(), name="admin_workout_list"),
 ]
--- admin_app/views.py
+++ admin_app/views.py
@@ -15,3 +15,28 @@ class UserList(View):
         users = User.objects.all()
 
         return render(request, self.template_name, {"users": users})
+
+
+class WorkoutList(View):
+    template_name = "admin_workout_list.html";
+# List workouts by user.id
+    def get(self, request, *args, **kwargs):
+        user_id = kwargs.get("user_id")
+        workout_list = models.Workout.objects.filter(user_id=user_id).order_by('id')
+        return render(request, self.template_name, {"workout_list": workout_list})
+
+
+class WorkoutExerciseList(View):
+# List workout_exercises by workout.id
+    def get(self, request, *args, **kwargs):
+        workout_id = kwargs.get("workout_id")
+        workout_exercise_list = models.WorkoutExercise.objects.filter(workout_id=workout_id).order_by('id')
+        return render(request, self.template_name, {"workout_exercise_list": workout_exercise_list})
+
+
+class ExerciseSetList(View):
+# List exercise_sets by workout_exercise.id
+    def get(self, request, *args, **kwargs):
+        workout_exercise_id = kwargs.get("workout_exercise_id")
+        exercise_set_list = models.ExerciseSet.objects.filter(workout_exercise_id=workout_exercise_id)
+        return render(request, self.template_name, {"exercise_set_list": exercise_set_list})
